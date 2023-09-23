7f1f9d2b722163ab81f6992d085126aec70a254e
Quberk <andiimran212@gmail.com>
2023-09-23 13:57:53 +0800

    Tambahan sedikit

--- frontend/src/components/Request BOQ/RemksPopUp.js
+++ frontend/src/components/Request BOQ/RemksPopUp.js
@@ -9,7 +9,16 @@ const RemksPopUp = ({ isOpen, togglePopup }) => {
                     <div className="modal-background"></div>
                     <div className="modal-content">
                         <div className="box">
-                            <p>Konten Popup Anda di sini.</p>
+                            <div className="field mt-4">
+                                <label className='label'>Remks</label>
+                                <div className="controls">
+                                    <input
+                                    type="text" 
+                                    className="input"
+                                    placeholder='Remks'
+                                    />
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <button className="modal-close is-large" aria-label="close" onClick={togglePopup}></button>
